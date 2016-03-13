ee0404049ca3acc1904fee01d49644c68a2ea660
Brig Young <brig@sonophotostudios.com>
2016-03-12 17:53:49 -0800

    Added a bulk print-test to helpfuncs.c

--- cli/help/d.c
+++ cli/help/d.c
@@ -4,10 +4,17 @@ int cmd_help_d(int argc, char ** argv, soarResult * res)
 {
 print("\
 NAME\n\
-d - Step SiOO [integer] decision cycles\n\
+ d - Step SiOO [integer] decision cycles\n\
 \n\
 SYNOPSIS\n\
-d [integer]\n\
+ d [integer]\n\
+\n\
+DESCRIPTION\n\
+ Runs the kernel for [integer] decision cycles. If no [integer] is\n\
+ given on the command line, 1 decision cycle is run.\n\
+\n\
+SEE ALSO\n\
+ e, run\n\
 \n\n");
 return 0;
 }
--- cli/help/helpfuncs.c
+++ cli/help/helpfuncs.c
@@ -31,3 +31,116 @@
 #include "version.c"
 #include "watch.c"
 
+
+/* This test should print out a notice of which function it is */
+/* printing */
+/* followed by the body of the help message */
+/* followed by a message of pass: or FAIL! based on return status */
+/* interestingly, these should always print "pass:" */
+
+int testhelpfuncs()
+{
+  int fail = 0;
+  int argc = 2;
+  char** argv;
+  soarResult* res;
+
+  print("Running cmd_help_d(): ");
+  fail = cmd_help_d(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_dirs(): ");
+  fail = cmd_help_dirs(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_e(): ");
+  fail = cmd_help_e(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_excise(): ");
+  fail = cmd_help_excise(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_help(): ");
+  fail = cmd_help_help(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_initsoar(): ");
+  fail = cmd_help_initsoar(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_io(): ");
+  fail = cmd_help_io(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_learn(): ");
+  fail = cmd_help_learn(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_log(): ");
+  fail = cmd_help_log(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_matches(): ");
+  fail = cmd_help_matches(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_memories(): ");
+  fail = cmd_help_memories(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_monitor(): ");
+  fail = cmd_help_monitor(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_popd(): ");
+  fail = cmd_help_popd(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_preferences(): ");
+  fail = cmd_help_preferences(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_print(): ");
+  fail = cmd_help_print(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_pwatch(): ");
+  fail = cmd_help_pwatch(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_quit(): ");
+  fail = cmd_help_quit(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_retenet(): ");
+  fail = cmd_help_retenet(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_run(): ");
+  fail = cmd_help_run(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_sioonews(): ");
+  fail = cmd_help_sioonews(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_sp(): ");
+  fail = cmd_help_sp(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_topd(): ");
+  fail = cmd_help_topd(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_version(): ");
+  fail = cmd_help_version(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+  print("Running cmd_help_watch(): ");
+  fail = cmd_help_watch(argc, argv, res);
+  (fail) ? print("FAIL!\n") : print("pass:\n");
+
+}
+
+
